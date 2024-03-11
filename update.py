0e9c733752c34b659b2666613905e452100a0eb2
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2024-03-11 17:31:28 +0900

    update.py one line rollback

--- update.py
+++ update.py
@@ -34,28 +34,21 @@ def main():
         
         if directory == '.':
             continue
-
-        if directory not in directories:
-            content+= ""
-
+            
         if directory not in directories:
             if directory in ["백준", "프로그래머스"]:
-                content += "## 📚 {}\n".format(directory)
+                content += "# 📚 {}\n".format(directory)
             else:
                 content += "### 🚀 {}\n".format(directory)
-                content += "| 문제 | 링크 | 문제 | 링크 |\n"
-                content += "| ----- | ----- | ----- | ----- |\n"
+                content += "| 문제 | 링크 |\n"
+                content += "| ----- | ----- |\n"
             directories.append(directory)
 
-        index = 0;
+        index = 0
         for file in files:
             if category not in solveds:
                 folder_link = parse.quote(os.path.join(root))
-                if index % 2 == 0:
-                    content += "|{}|[링크]({})|".format(category, folder_link)
-                else:
-                    content += "{}|[링크]({})|\n".format(category, folder_link)
-                index += 1;
+                content += "|{}|[링크]({})|\n".format(category, folder_link)
                 solveds.append(category)
                 print("category : " + category)
 
