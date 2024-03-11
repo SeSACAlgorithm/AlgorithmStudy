2bf8c9c6b087ded0ad4ed5e2b23518c65272cb02
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2024-03-11 16:03:27 +0900

    목차 test

--- update.py
+++ update.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import os
 from urllib import parse
 
@@ -7,6 +5,31 @@ HEADER="""#
 # 백준 & 프로그래머스 문제 풀이 목록
 
 """
+
+def generate_table_of_contents(content):
+    toc = "## 목차\n"
+    toc += "| 카테고리 | 문제 | 링크 |\n"
+    toc += "| ----- | ----- | ----- |\n"
+    
+    for root, dirs, files in os.walk("."):
+        dirs.sort()
+        if root == '.':
+            for dir in ('.git', '.github'):
+                try:
+                    dirs.remove(dir)
+                except ValueError:
+                    pass
+            continue
+
+        category = os.path.basename(root)
+        if category in ['.', 'images']:
+            continue
+
+        for file in files:
+            folder_link = parse.quote(os.path.join(root))
+            toc += "|{}|{}|[링크]({})|\n".format(category, file, folder_link)
+    
+    return toc
 
 def main():
     content = ""
@@ -42,8 +65,6 @@ def main():
                 content += "## 🚀 {}\n".format(directory)
                 content += "| 문제 | 링크 |\n"
                 content += "| ----- | ----- |\n"
-                #content += "| 문제 | 링크 | 문제 | 링크 |\n"
-                #content += "| ----- | ----- | ----- | ----- |\n"
             directories.append(directory)
 
         index = 0;
@@ -51,13 +72,11 @@ def main():
             if category not in solveds:
                 folder_link = parse.quote(os.path.join(root))
                 content += "|{}|[링크]({})|\n".format(category, folder_link)
-                #if index % 2 == 0:
-                #    content += "|{}|[링크]({})|".format(category, folder_link)
-                #else:
-                #    content += "{}|[링크]({})|\n".format(category, folder_link)
                 solveds.append(category)
                 print("category : " + category)
                 index += 1;
+
+    content += generate_table_of_contents(content)
 
     with open("README.md", "w") as fd:
         fd.write(content)
