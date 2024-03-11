0644d97fe5061e25a4801767e6e424d47a583800
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2024-03-11 14:05:30 +0900

    Update update.py

--- update.py
+++ update.py
@@ -44,19 +44,19 @@ def main():
                 content += "| ----- | ----- | ----- | ----- |\n"
             directories.append(directory)
 
-        # 모든 목록이 두 개씩 출력되도록 수정
-        for i in range(0, len(files), 2):
-            if category not in solveds:
-                row_content = ""
-                for j in range(2):  # 한 행에 두 개씩 출력
-                    file_index = i + j
-                    if file_index < len(files):
-                        row_content += "|{}|[링크]({})|".format(category, parse.quote(os.path.join(root, files[file_index])))
-                    else:  # 파일이 없는 경우 비어있는 열 추가
-                        row_content += "| | |"
-                content += row_content + "\n"
-                solveds.append(category)
-                print("category : " + category)
+            # 모든 목록이 두 개씩 출력되도록 수정
+            for i in range(0, len(files), 2):
+                if category not in solveds:
+                    row_content = ""
+                    for j in range(2):  # 한 행에 두 개씩 출력
+                        file_index = i + j
+                        if file_index < len(files):
+                            row_content += "|{}|[링크]({})|".format(category, parse.quote(os.path.join(root, files[file_index])))
+                        else:  # 파일이 없는 경우 비어있는 열 추가
+                            row_content += "| | |"
+                    content += row_content + "\n"
+                    solveds.append(category)
+                    print("category : " + category)
 
     with open("README.md", "w") as fd:
         fd.write(content)
