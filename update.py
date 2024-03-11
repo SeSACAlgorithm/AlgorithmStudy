31932842552f8cf8f88b652549169d206ebc4146
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2024-03-11 16:16:24 +0900

    update.py

--- update.py
+++ update.py
@@ -48,13 +48,13 @@ def main():
         for file in files:
             if category not in solveds:
                 folder_link = parse.quote(os.path.join(root))
-                content += "|{}|[링크]({})|\n".format(category, folder_link)
-                #if index % 2 == 0:
-                #    content += "|{}|[링크]({})|".format(category, folder_link)
-                #else:
-                #    content += "{}|[링크]({})|\n".format(category, folder_link)
-                solveds.append(category)
-                print("category : " + category)
+                #content += "|{}|[링크]({})|\n".format(category, folder_link)
+                if index % 2 == 0:
+                    content += "|{}|[링크]({})|".format(category, folder_link)
+                else:
+                    content += "{}|[링크]({})|\n".format(category, folder_link)
+                    solveds.append(category)
+                    print("category : " + category)
                 index += 1;
 
     with open("README.md", "w") as fd:
