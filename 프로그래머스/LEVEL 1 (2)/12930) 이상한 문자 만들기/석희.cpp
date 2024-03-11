985a27920c5a2dca857447a54b18be42b7bf18c6
Hyun_W <69100145+yehang218@users.noreply.github.com>
2024-03-12 01:26:23 +0900

    Pro)12930 - 석

---
+++ 프로그래머스/LEVEL 1 (2)/12930) 이상한 문자 만들기/석희.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+string solution(string s)
+{
+    string answer = "";
+    int index = 0;
+
+    for (auto cAlphabet : s)
+    {
+        if (cAlphabet == ' ')
+        {
+            index = -1;
+        }
+        answer += (index % 2) ? tolower(cAlphabet) : toupper(cAlphabet);
+        index++;
+    }
+
+    return answer;
+}
