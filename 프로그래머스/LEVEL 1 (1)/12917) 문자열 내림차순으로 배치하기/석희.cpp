a4c54f237af1bf62fa6bb911133053892756c545
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2024-03-14 13:51:21 +0900

    Pro) 12917 - sh

---
+++ 프로그래머스/LEVEL 1 (1)/12917) 문자열 내림차순으로 배치하기/석희.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s)
+{
+    string answer = "";
+    vector<char> v_char;
+    for (auto c : s)
+    {
+        v_char.push_back(c);
+    }
+    sort(v_char.begin(), v_char.end(), greater<>());
+    for (auto c : v_char)
+    {
+        answer += c;
+    }
+    return answer;
+}
+int main(void)
+{
+    cout << solution("Zbcdefg");
+}
