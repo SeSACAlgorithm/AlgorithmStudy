b18a7095af324b0656d9db79fc5bc63dc7b1eacc
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2024-03-19 16:42:10 +0900

    Update update.py

--- update.py
+++ update.py
@@ -3,7 +3,7 @@
 import os
 from urllib import parse
 
-HEADER = """# 
+HEADER="""# 
 # 백준 & 프로그래머스 문제 풀이 목록
 
 ## 목차
@@ -12,26 +12,12 @@ HEADER = """#
 
 """
 
-# 추가된 이름 체크 배열
-NAME_CHECK = {
-    "윤지": False,
-    "석희": False,
-    "경호": False,
-    "정완": False,
-    "윤선": False,
-    "응찬": False
-}
-
-def update_name_check(name, value):
-    if name in NAME_CHECK:
-        NAME_CHECK[name] = value
-
 def main():
     content = ""
     content += HEADER
-
-    directories = []
-    solveds = []
+    
+    directories = [];
+    solveds = [];
 
     for root, dirs, files in os.walk("."):
         dirs.sort()
@@ -44,15 +30,15 @@ def main():
             continue
 
         category = os.path.basename(root)
-
+        
         if category == 'images':
             continue
-
+        
         directory = os.path.basename(os.path.dirname(root))
-
+        
         if directory == '.':
             continue
-
+            
         if directory not in directories:
             if directories:
                 content += "</details>\n\n"
@@ -63,37 +49,29 @@ def main():
                 content += "  <summary><b>"
                 content += "🚀 {}</b></summary>\n\n".format(directory)
                 content += "| 문제 | 링크 | 윥 | 석 | 경 | 정 | 윤 | 응 |\n"
-                content += "| ----- | ----- | ----- | ----- | ----- | ----- | ----- | ----- |\n"
+                content += "| ----- | ----- | ----- | ----- | ----- | ----- | ----- | ----- |"
             directories.append(directory)
 
         files_count = len(files)
         for file in files:
-            for name in NAME_CHECK.keys():
-                if name in file:
-                    update_name_check(name, True)
             if category not in solveds:
-                for name in NAME_CHECK:
-                    update_name_check(name, False)
-                for name in NAME_CHECK.keys():
-                    if name in file:
-                        update_name_check(name, True)
                 folder_link = parse.quote(os.path.join(root))
-                content += "|{}|[링크]({})|".format(category, folder_link)
+                content += "\n|{}|[링크]({})|".format(category, folder_link)
                 solveds.append(category)
-                for name, checked in NAME_CHECK.items():  # 이름 체크 배열 확인
-                    if checked:
-                        content += "✔|"  # 해당하는 이름에 ✔ 표시
-                    else:
-                        content += " |"  # 해당하는 이름에 공백 처리
-                content += "\n"
-                print("category : " + category)
+            names = ['윤지', '석희', '경호', '정완', '윤선', '응찬']
+            for name in names:
+                if name in file:
+                    content += "✔"
+                else:
+                    content += ""
+                content += "|"
 
+                
     if directories:  # Check if there are any directories
         content += "</details>\n\n"  # Close the last details tag
 
     with open("README.md", "w") as fd:
         fd.write(content)
-
-
+        
 if __name__ == "__main__":
     main()
