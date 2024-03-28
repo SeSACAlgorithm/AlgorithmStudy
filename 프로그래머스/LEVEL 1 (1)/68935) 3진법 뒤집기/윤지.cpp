69e1fd67a2617e664b325c65409495c689a874a5
NIJJY7 <102662924+NIJJY7@users.noreply.github.com>
2024-03-28 20:28:49 +0900

    Create 윤지.cpp

---
+++ 프로그래머스/LEVEL 1 (1)/68935) 3진법 뒤집기/윤지.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include<cmath>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    string str;
+    
+    while(1){
+        str += to_string(n%3);
+        if(n<3) break;
+        n = n/3;
+        
+    }
+    
+    for(int i=0; i<str.size(); i++){
+        answer += pow(3,str.size()-i-1) * (str[i]-'0');
+    }
+    
+    return answer;
+}
