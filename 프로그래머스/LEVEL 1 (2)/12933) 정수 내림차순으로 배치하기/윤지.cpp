daa74162de93ab3a3ff051a11c0b1b8ee3c4ee5f
NIJJY7 <102662924+NIJJY7@users.noreply.github.com>
2024-04-02 13:46:54 +0900

    Create 윤지.cpp

---
+++ 프로그래머스/LEVEL 1 (2)/12933) 정수 내림차순으로 배치하기/윤지.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    string a = to_string(n);
+    sort(a.begin(),a.end(),greater<>());
+    answer = stol(a);
+    
+    return answer;
+}
