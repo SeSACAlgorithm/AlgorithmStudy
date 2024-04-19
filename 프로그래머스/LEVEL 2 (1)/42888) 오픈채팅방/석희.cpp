0081ce89886453a39106eba96591d0d633a0f0d4
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2024-04-19 14:57:57 +0900

    Pro) 42888 - sh 주석 추가

--- 프로그래머스/LEVEL 2 (1)/42888) 오픈채팅방/석희.cpp
+++ 프로그래머스/LEVEL 2 (1)/42888) 오픈채팅방/석희.cpp
@@ -1,4 +1,23 @@
-#include <iostream>
+// 문제 분석
+/*
+들어오고 나간거 answer에 담기
+change하면 닉네임이 바뀜 / 다시 들어올 때 닉네임 바꾸면 바뀜
+=> 최종적으로 알림을 찍을 때 최종 닉네임으로 나와야함!
+
+유의해야할 케이스
+record = ["Enter uid1234 Rion", "Change uid1234 Hi", "Leave uid1234", "Enter uid1234 Prodo"]
+answer = ["Prodo님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]
+
+라이언 들어옴
+라이언 닉변 -> 하이
+라이언 나감
+라이언 -> 프로도로 바꿔서 들어옴
+
+프로도 들어옴
+프로도 나감
+프로도 들어옴
+*/
+
 #include <string>
 #include <vector>
 #include <map>
@@ -49,9 +68,4 @@ vector<string> solution(vector<string> record) {
     }
 
     return answer;
-}
-
-int main(void)
-{
-
 }
