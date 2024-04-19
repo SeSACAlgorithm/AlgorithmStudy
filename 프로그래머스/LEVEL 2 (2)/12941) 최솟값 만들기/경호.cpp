252a88d33543885644e342ce65b516eb09cd3499
kyungho Park <99066633+pkh0308@users.noreply.github.com>
2024-04-19 15:32:37 +0900

    Add files via upload

---
+++ 프로그래머스/LEVEL 2 (2)/12941) 최솟값 만들기/경호.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), [](int a, int b) { return a > b; });
+    
+    int answer = 0;
+    for(int i = 0; i < A.size(); i++)
+        answer += A[i] * B[i];
+    
+    return answer;
+}
