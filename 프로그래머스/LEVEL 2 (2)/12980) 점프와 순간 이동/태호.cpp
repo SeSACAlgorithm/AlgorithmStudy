ead8d44e1415084b68ea56246db56a141702bf5a
YunTaeHo <84884775+YunTaeHo@users.noreply.github.com>
2024-04-23 10:45:41 +0900

    이것보다 줄 더 적게 하면 100000000000억줌

--- 프로그래머스/LEVEL 2 (2)/12980) 점프와 순간 이동/태호.cpp
+++ 프로그래머스/LEVEL 2 (2)/12980) 점프와 순간 이동/태호.cpp
@@ -1,9 +1,1 @@
-#include <iostream>
-using namespace std;
-
-int solution(int n)
-{
-    int answer = 0;
-    while (n){ if(n % 2 == 1){ ++answer; n = (n - 1) / 2;} else n = n / 2;}
-    return answer;
-}
+#include <iostream> using namespace std; int solution(int n) { int answer = 0; while (n){ if(n % 2 == 1){ ++answer; n = (n - 1) / 2;} else n = n / 2;}return answer; }
