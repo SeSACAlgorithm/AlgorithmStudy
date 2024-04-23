fa817fdf344f9284c62103771fe0e55680d5c43e
YunTaeHo <84884775+YunTaeHo@users.noreply.github.com>
2024-04-23 10:44:35 +0900

    이것보다 더 줄이면 100억

---
+++ 프로그래머스/LEVEL 2 (2)/12980) 점프와 순간 이동/태호.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+    while (n){ if(n % 2 == 1){ ++answer; n = (n - 1) / 2;} else n = n / 2;}
+    return answer;
+}
