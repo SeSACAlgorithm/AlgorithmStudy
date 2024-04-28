e7d05a8bc8eac5d549e130cb52fdbc4c785d6495
NIJJY7 <102662924+NIJJY7@users.noreply.github.com>
2024-04-28 22:32:13 +0900

    Update 윤지.py

--- 백준/그래프 탐색/2178) 미로 탐색/윤지.py
+++ 백준/그래프 탐색/2178) 미로 탐색/윤지.py
@@ -1,45 +1,38 @@
+from pprint import pprint
 from collections import deque
 
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-T = int(input())
-for test_case in range(1, T + 1):
-    N = int(input())
-    maze = [list(map(int,  input())) for _ in range(N)]
+N, M = map(int, input().split())
 
-    # 최단 거리를 구하는 문제 -> BFS -> QUEUE 사용
-    queue = deque()
-    visited = set()
+arr = [list(map(int, input())) for _ in range(N)]
 
+# 기본 셋팅
+queue = deque()     #방문 예정지
+visited = set()     #방문지
+ans = 1
 
-    for r in range(N):
-        for c in range(N):
-            if maze[r][c] == 2: #출발지 찾기
-                queue.append((-1, r, c))
-                break
+# 시작 방문 예정지 설정
+queue.append((0, 0, ans))
 
-    ans = 0
-    while queue:
-        # 방문
-        cnt, r, c = queue.popleft()
-        visited.add((r, c))
+# 방문 예정지가 빌 때까지 loop
+while queue:
+    # 방문
+    x, y, ans = queue.popleft()
 
-        # 도착지면 종료 -> 도착까지의 거리 리턴
-        if maze[r][c] == 3:
-            ans = cnt
-            break
+    # 종료 조건
+    if x == N-1 and y == M-1:
+        print(f"{ans}")
+        break
 
-        # 탐색
-        # 델타 탐색
-        for d in range(4):
-            nr, nc = r + dr[d], c + dc[d]
+    for d in range(4):
+        nx, ny = x + dx[d], y + dy[d]
 
-            # 범위를 벗어날 때, 방문 안한 곳을 때, 벽이 아닐 때
-            if 0 <= nr < N and 0 <= nc < N and (nr, nc) not in visited and maze[nr][nc] != 1:
-                # 좌표 외에, 방문을 하면서 지나간 거리를 큐에 담음
-                queue.append((cnt + 1, nr, nc))
-                visited.add((nr, nc))
+        # 갈수 있는 경로만 탐색
+        if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 1 and (nx, ny) not in visited:
+            queue.append((nx, ny, ans+1))
+            
+            # 여기에서 방문 표기 해야 함 -> 시간초과 방지
+            visited.add((nx, ny))
 
-    print(f'#{test_case} {ans}')
-    
