0f6e004ad561be6c94e41fdc1c561db41f0b876d
jwk0495 <jwk0495@naver.com>
2024-04-29 21:53:15 +0900

    Create 정완.cpp

---
+++ 백준/동적 계획법 1/10870) 피보나치 수 5/정완.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int Fibonacci[21];
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    Fibonacci[0] = 0;
+    Fibonacci[1] = 1;
+
+    for(int i=2; i<=n; i++)
+        Fibonacci[i] = Fibonacci[i-1] + Fibonacci[i-2];
+
+    cout << Fibonacci[n];
+}
