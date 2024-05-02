77356ef52f91c330092ddabd05cec88cce16f93d
jwk0495 <jwk0495@naver.com>
2024-05-02 21:11:18 +0900

    Update 정완.cpp

--- 프로그래머스/LEVEL 1 (1)/86491) 최소직사각형/정완.cpp
+++ 프로그래머스/LEVEL 1 (1)/86491) 최소직사각형/정완.cpp
@@ -12,14 +12,14 @@ int solution(vector<vector<int>> sizes) {
     
     for(vector<int> size : sizes)
     {
-        max1.push_back(max(size[0], size[1]));
-        min1.push_back(min(size[0], size[1]));
+        max1.push_back(max(size[0], size[1]));      // 가로 중 최대값 선별
+        min1.push_back(min(size[0], size[1]));      // 세로 중 최대값 선별
     }
     
-    sort(max1.begin(), max1.end(), greater<int>());
+    sort(max1.begin(), max1.end(), greater<int>()); // 정렬
     sort(min1.begin(), min1.end(), greater<int>());
     
-    ans = max1.front() * min1.front();
+    ans = max1.front() * min1.front();              // 정렬된 값 맨 처음 곱
     
     return ans;
 }
