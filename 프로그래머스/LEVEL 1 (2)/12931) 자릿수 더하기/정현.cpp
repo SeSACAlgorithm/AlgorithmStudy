12d160d6d1e6ae00939337242e6eae43c7e9e679
Kwon512 <155418784+Kwon512@users.noreply.github.com>
2024-05-02 13:31:30 +0900

    20240502

--- 프로그래머스/LEVEL 1 (2)/12931) 자릿수 더하기/정현.cpp
+++ 프로그래머스/LEVEL 1 (2)/12931) 자릿수 더하기/정현.cpp
@@ -3,23 +3,23 @@
 
 using namespace std;
 
-// 이건 내가 함
-//int solution(int n)
-//{
-//    int answer = 0;
-//
-//    while (n != 0) // n 이 0이 아닐 때까지 반복해서 계속 일의 자리수 더하자
-//    {
-		 // 자리수 별로 하나씩 answer에 더하기
-         // 일의 자리수 하나씩 더하자
-//        answer += n % 10; // 일의 자리 더하고
-//        n /= 10; // 더해준 일의 자리 버려
-//    }
-//    return answer;
-//}
+ //이건 내가 함
+int solution(int n)
+{
+    int answer = 0;
+
+    while (n != 0) // n 이 0이 아닐 때까지 반복해서 계속 일의 자리수 더하자
+    {
+		  //자리수 별로 하나씩 answer에 더하기
+          //일의 자리수 하나씩 더하자
+        answer += n % 10; // 일의 자리 더하고
+        n /= 10; // 더해준 일의 자리 버려
+    }
+    return answer;
+}
 
 // 윤지 언니꺼 봄
-int solution(int n)
+int solution2(int n)
 {
 	int ans = 0;
 
