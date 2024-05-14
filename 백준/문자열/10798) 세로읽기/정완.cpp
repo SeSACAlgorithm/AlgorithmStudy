f31152f0762c065147b96fc4c047080a8440dbae
jwk0495 <jwk0495@naver.com>
2024-05-14 09:33:46 +0900

    Create 정완.cpp

---
+++ 백준/문자열/10798) 세로읽기/정완.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string word[5];
+
+    for(int i=0; i<5; i++)
+        cin >> word[i];
+
+    for(int i=0; i<15; i++)
+        for(int j=0; j<5; j++)
+            if(word[j].size() > i)  // 공백 아닐 때
+                cout << word[j][i];
+    
+    return 0;
+}
