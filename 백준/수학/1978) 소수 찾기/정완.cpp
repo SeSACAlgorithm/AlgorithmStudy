a95aff276d58a264e6c4b460b525d0369078b5cf
jwk0495 <jwk0495@naver.com>
2024-05-16 18:34:19 +0900

    Create 정완.cpp

---
+++ 백준/수학/1978) 소수 찾기/정완.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, cnt, num, result = 0;
+    cin >> N;
+
+
+    for(int i=0; i<N; i++)
+    {
+        cin >> num;
+
+        for(int j=1; j<=num; j++)
+            if(num % j == 0)
+                cnt++;
+
+        if(cnt == 2)
+            result++;
+        cnt = 0;
+    }
+
+    cout << result << '\n';
+}
