faf8c4810aa245ffc4255f13c2b79772c229d9bf
Kwon512 <155418784+Kwon512@users.noreply.github.com>
2024-05-22 10:19:08 +0900

    Create 정현.cpp

---
+++ 프로그래머스/LEVEL 1 (2)/12943) 콜라츠 추측/정현.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+	int answer = 0;
+	long n = num;
+
+	while (n != 1)
+	{
+		n = n % 2 == 0 ? n / 2 : n = n * 3 + 1;
+		answer++;
+
+		if (answer == 500)
+		{
+			return -1;
+			break;
+		}
+	}
+
+	return answer;
+}
