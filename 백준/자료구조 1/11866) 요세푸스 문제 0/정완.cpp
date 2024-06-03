9bb8d1ca4ed065141189a3ca1fbf4d8141baf7b9
jwk0495 <jwk0495@naver.com>
2024-06-03 18:45:07 +0900

    Create 정완.cpp

---
+++ 백준/자료구조 1/11866) 요세푸스 문제 0/정완.cpp
@@ -0,0 +1,43 @@
+#include "iostream"
+#include "queue"
+
+using namespace std;
+
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+
+    queue<int> q;
+
+    for(int i=1; i<=N; i++)
+        q.push(i);
+
+    int count = 1;
+
+    cout << "<";
+    
+    while (!q.empty())
+    {
+        if(count % K == 0)
+        {
+            int ans = q.front();
+            q.pop();
+
+            if(!q.empty())
+                cout << ans << ", ";
+
+            else
+                cout << ans << ">";
+        }
+
+        else
+        {
+            int num = q.front();
+            q.pop();
+            q.push(num);
+        }
+        count++;
+    }
+}
+
