97daf44da12c997bd6b7098fcef36a238203b402
Kwon <gksksla9900@naver.com>
2024-06-17 00:49:57 +0900

    Update 정현.cpp

--- 프로그래머스/LEVEL 1 (1)/12903) 가운데 글자 가져오기/정현.cpp
+++ 프로그래머스/LEVEL 1 (1)/12903) 가운데 글자 가져오기/정현.cpp
@@ -5,17 +5,15 @@ using namespace std;
 
 string solution(string s) {
     string answer = "";
-
-    if (s.size() % 2 == 0) {
-        answer = s[s.size() / 2 - 1];
-        answer += s[s.size() / 2];
-
+    //짝수일 경우 가운데 두글자
+    //홀수일 경우 가운데 한글자 <- 이건 짝수든 홀수든 무조건 추가해야함
+    // 짝수일 경우
+    if (s.size() % 2 == 0)
+    {
+        // 가운데 첫번째 글자
+        answer += s[s.size() / 2 - 1];
     }
-    else {
-        answer = s[s.size() / 2];
-    }
-
-
-
+    // 가운데 글자
+    answer += s[s.size() / 2];
     return answer;
 }
