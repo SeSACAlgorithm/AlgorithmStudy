9d57706f99fbad1d041460f6e07194dd291e3fe7
jwk0495 <jwk0495@naver.com>
2024-06-19 11:14:21 +0900

    Create 정완.cpp

---
+++ 백준/탐욕법(Greedy)/11047) 동전0/정완.cpp
@@ -0,0 +1,26 @@
+#include "iostream"
+
+using namespace std;
+
+int coin[11];
+
+int main()
+{
+    int n, k, ans = 0;
+
+    cin >> n >> k;
+
+    for(int i=0; i<n; i++)
+        cin >> coin[i];
+
+    for(int i=n-1; i>=0; i--)
+    {
+        if(k == 0)
+            break;
+
+        ans += k/coin[i];
+        k %= coin[i];
+    }
+
+    cout << ans;
+}
