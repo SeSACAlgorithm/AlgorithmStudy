36dacccb0e73204097e94675ce96d105b361e4fd
jwk0495 <jwk0495@naver.com>
2024-07-01 10:52:49 +0900

    Create 정완.cpp

---
+++ 백준/동적 계획법 1/1463) 1로 만들기/정완.cpp
@@ -0,0 +1,32 @@
+#include "iostream"
+#include "algorithm"
+
+using namespace std;
+
+int d[1000001];
+int N;
+
+int solve(int N)
+{
+    d[1] = 0;
+    for(int i =2; i<=N; i++)
+    {
+        d[i] = d[i-1] + 1;
+
+        if(i % 2 == 0)
+            d[i] = min(d[i], d[i / 2] + 1);
+
+        if(i % 3 == 0)
+            d[i] = min(d[i], d[i / 3] + 1);
+    }
+
+    cout << d[N];
+}
+
+
+int main(void)
+{
+    cin >> N;
+    
+    solve(N);
+}
