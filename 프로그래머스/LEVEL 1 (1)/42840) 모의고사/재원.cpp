6134a1228c13e2aa5333cdb301fe2ee51f3a65f4
choi06151 <78068768+choi06151@users.noreply.github.com>
2024-07-12 08:55:05 +0900

    Update 재원.cpp

--- 프로그래머스/LEVEL 1 (1)/42840) 모의고사/재원.cpp
+++ 프로그래머스/LEVEL 1 (1)/42840) 모의고사/재원.cpp
@@ -37,6 +37,4 @@ vector<int> solution(vector<int> answers) {
     return answer;
 }
 
-본 콘텐츠를 무단으로 복사 후 게재 혹은 배포할 시 저작권법에 의거하여 처벌받을 수 있음을 명시합니다.
-출처 : https://ssocoit.tistory.com/33 [코딩하는 경제학도]
-Copyright © SsocoIT
+
