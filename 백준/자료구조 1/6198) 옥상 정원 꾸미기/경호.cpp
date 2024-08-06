823785b987e110add637347d9a9765a35fedf373
kyungho Park <99066633+pkh0308@users.noreply.github.com>
2024-08-06 10:31:35 +0900

    Add files via upload

---
+++ 백준/자료구조 1/6198) 옥상 정원 꾸미기/경호.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> buildings(N);
+    for (int i = 0; i < N; ++i)
+        cin >> buildings[i];
+
+    stack<int> taller;
+    long long answer = 0;
+	for(int i = 0; i < N; ++i)
+    {
+        // 스택이 비어있다면 추가 후 스킵
+		if(taller.empty())
+		{
+			taller.push(buildings[i]);
+            continue;
+	    }
+        // 스택에서 현재 만난 건물 높이 이하의 빌딩은 모두 제거
+        while(false == taller.empty() && taller.top() <= buildings[i])
+            taller.pop();
+        // 스택에 남아있는 건물 수 만큼 결과 값 증가, 이후 현재 빌딩 스택에 추가
+        answer += taller.size();
+        taller.push(buildings[i]);
+    }
+
+    cout << answer;
+}
