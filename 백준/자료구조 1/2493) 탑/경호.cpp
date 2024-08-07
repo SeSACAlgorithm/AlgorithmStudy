45f15ef39e66ce2db6e6c264daa0fc03fb98c25e
kyungho Park <99066633+pkh0308@users.noreply.github.com>
2024-08-07 10:43:20 +0900

    Update 경호.cpp

--- 백준/자료구조 1/2493) 탑/경호.cpp
+++ 백준/자료구조 1/2493) 탑/경호.cpp
@@ -12,25 +12,25 @@ int main()
     stack<pair<int, int>> lasers; // first: height / second: number
     vector<int> answer(N, 0);
     int temp;
-	for(int i = 0; i < N; ++i)
+    for(int i = 0; i < N; ++i)
     {
         cin >> temp;
 
         // 스택이 비어있다면 추가 후 스킵
-		if(lasers.empty())
-		{
+	if(lasers.empty())
+	{
             lasers.emplace(temp, i + 1);
             continue;
-	    }
+	}
         // 스택에서 현재 만난 건물 높이보다 낮은 건물은 모두 제거
         while(false == lasers.empty() && lasers.top().first < temp)
             lasers.pop();
         // 스택에 값이 남아있다면 해당 건물의 번호를 저장하고, 이후 현재 빌딩 스택에 추가
         if(false == lasers.empty())
-			answer[i] = lasers.top().second;
+	    answer[i] = lasers.top().second;
         lasers.emplace(temp, i + 1);
     }
 
     for(const int& ans : answer)
-		cout << ans << " ";
+	cout << ans << " ";
 }
