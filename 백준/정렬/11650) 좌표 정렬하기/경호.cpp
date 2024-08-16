d0b36641cc6889e9e29e2f93edc04c200d190ca5
kyungho Park <99066633+pkh0308@users.noreply.github.com>
2024-08-16 16:45:38 +0900

    Add files via upload

---
+++ 백준/정렬/11650) 좌표 정렬하기/경호.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct cmp
+{
+	bool operator()(pair<int, int> a, pair<int, int> b) const
+    {
+        return a.first == b.first ? a.second > b.second : a.first > b.first;
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
+
+    int x, y;
+    for(int i = 0; i < N; ++i)
+    {
+        cin >> x >> y;
+        pq.emplace(x, y);
+    }
+
+    while(false == pq.empty())
+    {
+        cout << pq.top().first << " " << pq.top().second << "\n";
+        pq.pop();
+    }
+}
