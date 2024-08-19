0dca3255267584302179bfd3a8cc1a23e6f3c35a
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2024-08-19 17:32:35 +0900

    [Docs] 리드미 자동화 코드 순위표

--- update.py
+++ update.py
@@ -95,7 +95,7 @@ def main():
         content += "\n</details>\n\n"  # Close the last details tag
 
     # 순위표 추가
-    most_common_names = name_count.most_common(5)  # 5등까지
+    most_common_names = name_count.most_common(7)  # 7등까지
     standings = "# 🏆 Ranking\n"
     standings += "| 🥇 순위 | 👤 이름 | ✔ 횟수 |\n"
     standings += "| --- | --- | --- |\n"
@@ -109,9 +109,12 @@ def main():
             medal = "🥉 3위"
         elif i == 4:
             medal = "🏅 4위"
-        else:
+        elif i == 5:
             medal = "🎖️ 5위"
-        
+        elif i == 6:
+            medal = "🎀 6위"
+        elif i == 7:
+            medal = "🎗 7위"
         standings += f"| {medal} | {name} | {count} |\n"
     
     content = HEADER + "\n" + standings + "\n" + content  # 목차 다음에 순위표 추가
