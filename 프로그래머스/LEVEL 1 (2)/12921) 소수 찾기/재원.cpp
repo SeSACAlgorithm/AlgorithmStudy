409228d0b24cdddb76fb15380bc9e95d5e5e99d5
choi06151 <78068768+choi06151@users.noreply.github.com>
2024-08-29 08:47:28 +0900

    Create 재원.cpp

---
+++ 프로그래머스/LEVEL 1 (2)/12921) 소수 찾기/재원.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool isPrime(int num) {
+    if (num < 2) return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int solution(int n) {
+    int answer = 0;
+    
+    for (int i = 2; i <= n; i++) {
+        if (isPrime(i)) {
+            answer++;
+        }
+    }
+    
+    return answer;
+}
