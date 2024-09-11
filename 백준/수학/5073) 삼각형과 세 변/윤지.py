825f712f8e1f4f9c16017c4fe68012515cf21a03
NIJJY7 <102662924+NIJJY7@users.noreply.github.com>
2024-09-11 23:03:47 +0900

    Create 윤지.py

---
+++ 백준/수학/5073) 삼각형과 세 변/윤지.py
@@ -0,0 +1,15 @@
+while 1:
+    a, b, c = map(int, input().split())
+
+    if a == 0 and b == 0 and c == 0:
+        break
+
+    if a + b <= c or a + c <= b or b + c <= a:
+        print("Invalid")
+    else:
+        if a == b == c:
+            print("Equilateral")
+        elif a == b or b == c or a == c:
+            print("Isosceles")
+        else:
+            print("Scalene")
