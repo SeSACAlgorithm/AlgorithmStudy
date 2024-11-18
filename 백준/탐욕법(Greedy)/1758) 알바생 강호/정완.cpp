87b4af4f41330458c00b0928174c6e6cff41e684
jwk0495 <155418911+jwk0495@users.noreply.github.com>
2024-11-18 19:15:31 +0900

    Create 정완.cpp

---
+++ 백준/탐욕법(Greedy)/1758) 알바생 강호/정완.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int N;
+vector<int> v;
+
+int main()
+{
+    cin >> N;
+
+    for(int i =0; i<N; i++)
+    {
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+
+    sort(v.rbegin(), v.rend());
+
+    long ans = 0;
+
+    for(int i=0; i<N; i++)
+        if(v[i] - i > 0)        // 팁이 음수가 아닌 경우 
+            ans += v[i] - i;    // v[i] - ((i + 1) - 1) => v[i] - i
+
+    cout << ans;
+}
