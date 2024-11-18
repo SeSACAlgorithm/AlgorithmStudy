1e34438e562b314b0f345d33eb77c2ff0a15b28f
jwk0495 <155418911+jwk0495@users.noreply.github.com>
2024-11-19 02:40:34 +0900

    Update 정완.cpp
    변수 이름 변경

--- 백준/탐욕법(Greedy)/20115) 에너지 드링크/정완.cpp
+++ 백준/탐욕법(Greedy)/20115) 에너지 드링크/정완.cpp
@@ -8,18 +8,18 @@ int main()
     int N;
     cin >> N;
 
-    float v[100001];        // 정수형 X, 실수형 O       float 나 double 사용
+    float e[100001];        // 정수형 X, 실수형 O       float 나 double 사용
     float max = 0;
 
     for(int i=0; i<N; i++)
-        cin >> v[i];
+        cin >> e[i];
 
-    sort(v, v + N);
+    sort(e, e + N);
 
-    max = v[N-1];
+    max = e[N-1];
 
     for(int i=0; i<N-1; i++)
-        max += (v[i] / 2);
+        max += (e[i] / 2);
 
     cout << max;
 }
