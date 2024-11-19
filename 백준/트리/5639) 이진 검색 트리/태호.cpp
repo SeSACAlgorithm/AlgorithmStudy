8f2e929ed1101f1c89da58108cea6d374800016c
YunTaeHo <84884775+YunTaeHo@users.noreply.github.com>
2024-11-19 10:52:33 +0900

    크리스마스

---
+++ 백준/트리/5639) 이진 검색 트리/태호.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    Node(Node* lc, Node* rc, int num)
+        : left(lc), right(rc), node(num) {};
+
+	Node* left;
+	Node* right;
+
+    int node;
+};
+
+struct BinaryTree 
+{
+private:
+    Node* root = nullptr;
+
+	Node* Insert(Node* current, Node* newNode)
+	{
+		if (current == nullptr)
+			return newNode;
+
+		if (current->node > newNode->node)
+			current->left = Insert(current->left, newNode);
+		else
+			current->right = Insert(current->right, newNode);
+
+        return current;
+	}
+
+	void PostOrder(Node* current)
+	{
+        if(current == nullptr)
+            return;
+
+        if(current->left)
+		    PostOrder(current->left);
+		
+        if(current->right)
+            PostOrder(current->right);
+
+		cout << current->node << '\n';
+	}
+
+public:
+    void MakeNode(int value)
+    {
+        root = Insert(root, new Node(nullptr, nullptr, value));
+    }
+
+    void CallPostOrder()
+    {
+        PostOrder(root);
+    }
+};
+
+
+int main() 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int node;
+    BinaryTree BT;
+
+
+    while (cin >> node)
+        BT.MakeNode(node);
+
+    BT.CallPostOrder();
+
+    return 0;
+}
