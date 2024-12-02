af0c093fd25fc1aecb4c34b436ccf38ae7a578ee
jwk0495 <155418911+jwk0495@users.noreply.github.com>
2024-12-03 01:19:44 +0900

    Update 정완.cpp

--- 백준/동적 계획법 1/2407) 조합/정완.cpp
+++ 백준/동적 계획법 1/2407) 조합/정완.cpp
@@ -13,52 +13,121 @@
 dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
 
 */
+
+// #include <iostream>
+// #include <string>
+// #include <algorithm>
+
+// using namespace std;
+
+// int main()
+// {
+//     int n, m;
+//     cin >> n >> m;
+
+//     string dp[102][102];    // 값이 커서 문자열로 처리
+
+//     for(int i=0; i<=n; i++)
+//         for(int j=0; j<=i; j++)
+//         {
+//             if(j == 0 || j == i)        // nC0 = nCn = 1
+//                 dp[i][j] = "1";
+
+//             else
+//             {
+//                 int sum = 0;
+//                 string n1 = dp[i-1][j];
+//                 string n2 = dp[i-1][j-1];
+
+//                 while (!n1.empty() || !n2.empty() || sum)
+//                 {
+//                     if(!n1.empty())
+//                     {
+//                         sum += n1.back() - '0';
+//                         n1.pop_back();
+//                     }
+
+//                     if(!n2.empty())
+//                     {
+//                         sum += n2.back() - '0';
+//                         n2.pop_back();
+//                     }
+
+//                     dp[i][j].push_back((sum % 10) + '0');
+//                     sum /= 10;
+//                 }
+                
+//                 reverse(dp[i][j].begin(), dp[i][j].end());
+//             }
+//         }
+
+//         cout << dp[n][m];
+// }
+
 #include <iostream>
 #include <string>
 #include <algorithm>
 
 using namespace std;
 
-int main()
+int n, m;
+string DP[102][102];
+
+void Input()
 {
-    int n, m;
     cin >> n >> m;
+}
 
-    string dp[102][102];    // 값이 커서 문자열로 처리
-
+void Solution()
+{
     for(int i=0; i<=n; i++)
+    {
         for(int j=0; j<=i; j++)
         {
-            if(j == 0 || j == i)        // nC0 = nCn = 1
-                dp[i][j] = "1";
-
+            if(j == 0 || j == i)
+                DP[i][j] = "1";
+            
             else
             {
                 int sum = 0;
-                string n1 = dp[i-1][j];
-                string n2 = dp[i-1][j-1];
+                string n1 = DP[i-1][j];
+                string n2 = DP[i-1][j-1];
 
-                while (!n1.empty() || !n2.empty() || sum)
+                while(!n1.empty() || !n2.empty() || sum)
                 {
                     if(!n1.empty())
                     {
-                        sum += n1.back() - '0';
+                        sum += n1.back() - '0';     // string --> int
                         n1.pop_back();
                     }
 
                     if(!n2.empty())
                     {
-                        sum += n2.back() - '0';
+                        sum += n2.back() - '0';     // string -->int
                         n2.pop_back();
                     }
-
-                    dp[i][j].push_back((sum % 10) + '0');
+                    
+                    DP[i][j].push_back((sum % 10) + '0');
                     sum /= 10;
                 }
-                
-                reverse(dp[i][j].begin(), dp[i][j].end());
+
+                reverse(DP[i][j].begin(), DP[i][j].end());
             }
         }
+    }
 
-        cout << dp[n][m];
+    cout << DP[n][m];
+}
+
+void Solve()
+{
+    Input();
+    Solution();
+}
+
+int main(void)
+{
+    Solve();
+
+    return 0;    
 }
